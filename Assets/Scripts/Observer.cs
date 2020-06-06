1a6f45619c31eec6314d1c1904e831704a77f894
mark-sawyer <alfador_321@hotmail.com>
2020-06-06 15:35:13 +1000

    Initial commit

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Observer : MonoBehaviour {
+    public GameObject vector;
+    public Complex[] zeroes;
+    public float timer;
+    private float[] angles;
+
+    public void Start() {
+        int numberOfZeroes = 4;
+        zeroes = new Complex[numberOfZeroes];
+        angles = new float[numberOfZeroes];
+        for (int i = 0; i < angles.Length; i++) {
+            angles[i] = Random.Range(0, 2 * Mathf.PI);
+        }
+
+        for (float i = -12f; i <= 12f; i = i + 0.2f) {
+            for (float j = -6f; j <= 6f; j = j + 0.2f) {
+                Instantiate(vector, new UnityEngine.Vector3(i, j, 0f), UnityEngine.Quaternion.identity);
+            }
+        }
+    }
+
+    public void Update() {
+        timer += Time.deltaTime;
+        for (int i = 0; i < zeroes.Length; i++) {
+            zeroes[i] += new Complex(Mathf.Cos(angles[i]), Mathf.Sin(angles[i])) * 0.01;
+            angles[i] += Random.Range(-0.1f, 0.1f);
+            if (zeroes[i].Magnitude > 10) {
+                angles[i] += Mathf.PI;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Vector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Vector : MonoBehaviour {
+    public SpriteRenderer sprite;
+    private Observer observer;
+    private Complex input;
+    private Complex output;
+    private Color defaultColour;
+    private float MAX_MAGNITUDE = 3;
+
+    void Start() {
+        observer = GameObject.Find("observer").GetComponent<Observer>();
+        input = new Complex(transform.position.x, transform.position.y);
+        defaultColour = new Color(0.9451f, 0.7373f, 0.2f);
+    }
+
+    void Update() {
+        sprite.color = defaultColour;
+        output = input - observer.zeroes[0];
+        for (int i = 1; i < observer.zeroes.Length; i++) {
+            output *= input - observer.zeroes[i];
+        }
+        output *= 0.001;
+        if (output.Magnitude < MAX_MAGNITUDE) {
+            transform.localScale = new UnityEngine.Vector3((float)output.Magnitude, (float)output.Magnitude * 0.5f, 1f);
+        }
+        else {
+            transform.localScale = new UnityEngine.Vector3(MAX_MAGNITUDE, MAX_MAGNITUDE / 2, 1f);
+            float difference = (float)output.Magnitude - MAX_MAGNITUDE;
+            sprite.color += new Color(0.01f * difference, -0.01f * difference, -0.01f * difference);
+        }
+        transform.eulerAngles = new UnityEngine.Vector3(0, 0, (float)output.Phase * Mathf.Rad2Deg);
+        transform.rotation *= UnityEngine.Quaternion.AngleAxis((Mathf.Sin(observer.timer) * 0.1f +  // waving
+                                                                   // moving field based on position
+                                                                   Mathf.Sin(transform.position.x * 0.5f + 0.1f * observer.timer) +
+                                                                   Mathf.Cos(0.5f * (transform.position.y + 0.1f * observer.timer))) *
+                                                                   Mathf.Rad2Deg +
+                                                                  2 * observer.timer,  // regular rotation
+                                                               UnityEngine.Vector3.forward);
+    }
+}
